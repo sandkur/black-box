6d2cee289c10d06a65087b25acc9b4b4f6fef2e0
sandy kurniawan <2decompile1@gmail.com>
2018-11-21 14:50:15 +0700

    DEPLOYYYY

---
+++ precache-manifest.8d2a5a5d288d5fe3fbe4ba9c197c555e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c3a05f4ca5d89d5af20",
+    "url": "/static/js/main.6c3a05f4.chunk.js"
+  },
+  {
+    "revision": "7d81c0bebca6204211fa",
+    "url": "/static/js/1.7d81c0be.chunk.js"
+  },
+  {
+    "revision": "6c3a05f4ca5d89d5af20",
+    "url": "/static/css/main.df6dc8c9.chunk.css"
+  },
+  {
+    "revision": "7d81c0bebca6204211fa",
+    "url": "/static/css/1.c2990450.chunk.css"
+  },
+  {
+    "revision": "5076b2e6b92c8dc0d260ee040a2ea644",
+    "url": "/index.html"
+  }
+];
